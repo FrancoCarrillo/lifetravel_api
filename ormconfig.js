d719044cb7d6c586254a96af06e7d671f2ad65a8
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-06-07 01:26:08 -0500

    Deploy Config heroku main

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
   type: 'mysql',
-  url: process.env.LIFETRAVEL_MYSQL,
+  url: 'mysql://bad154a7c5b577:c69fab62@us-cdbr-east-05.cleardb.net/heroku_0e1eb01b6f860d2?reconnect=true',
   migrationsRun: true,
   logging: true,
   timezone: '+0',
